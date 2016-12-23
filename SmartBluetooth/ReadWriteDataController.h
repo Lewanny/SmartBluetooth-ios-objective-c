52a6e32cf167e50b3066e5574ac6ec973abfa1af
Lewanny <1016201450@qq.com>
2016-12-23 13:50:10 +0800

    Modify the comment format

--- SmartBluetooth/ReadWriteDataController.h
+++ SmartBluetooth/ReadWriteDataController.h
@@ -1,10 +1,18 @@
-//
-//  ReadWriteDataController.h
-//  SmartBluetooth
-//
-//  Created by 李宏远 on 2016/12/22.
-//  Copyright © 2016年 SmartCodeUnited. All rights reserved.
-//
+/*
+ * Copyright (C) 2016 SmartCodeUnited http://www.smartcodeunited.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import <UIKit/UIKit.h>
 #import "SCUBluetoothDeviceManager.h"
